d9b45c93da0eafe37ea3db168d5d6f46548a6bc2
NeilGGopal <gopaln@purdue.edu>
2022-10-29 17:24:07 -0400

    Enabled backend to connect to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config({ path: './config.env' })
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 app.use(cors())
 app.use(express.json())
-app.use(require('./routes/record'))
+app.use(require('./routes/record.js'))
 // get driver connection
 const dbo = require('./db/conn')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   // perform a database connection when server starts
