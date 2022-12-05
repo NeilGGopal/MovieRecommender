2c1aa8c1227dbead4e4ab3ee7f1dbdfb6419be9b
NeilGGopal <gopaln@purdue.edu>
2022-12-05 17:46:43 -0500

    Created sample form in Planner

--- react-app/src/components/Planner.js
+++ react-app/src/components/Planner.js
@@ -10,6 +10,14 @@ function Planner() {
          <button class="btn btn-outline-danger">Remove Movie</button>
          &nbsp;
          &nbsp;
+          <form>
+            <label for="mname">Movie Name:</label><br></br>
+            <input id="mname" type="text"></input><br></br>
+            <label for="mlength">Movie Length (Minutes):</label><br></br>
+            <input id="mlength" type="text"></input><br></br>
+            <label for="myear">Movie Year</label><br></br>
+            <input id="myear" type="text"></input><br></br>
+          </form>
          <button class="btn btn-outline-danger">Add Movie</button>
     </div>
   ); 
