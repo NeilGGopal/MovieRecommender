f1c582f1d6e518a4c99ae11418f296cd21e56baf
NeilGGopal <gopaln@purdue.edu>
2022-12-05 18:09:31 -0500

    Implemented a create rating endpoint

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.json({ extended: false }))
 // app.use('/api/movieModel', require('./api/movie'))
 const Movie = require('./db/movie')
 const Actor = require('./db/actor')
+const Rating = require('./db/rating')
 // get driver connection
 const dbo = require('./db/conn')
 
@@ -31,6 +32,15 @@ app.post('/create-actor', async (req, res) => {
   const actorModel = new Actor(actor)
   await actorModel.save()
   res.json(actorModel)
+})
+
+app.post('/create-rating', async (req, res) => {
+  const rating = {}
+  rating.imdb_rating = req.body.imdb_rating
+  rating.personal_rating = req.body.personal_rating
+  const ratingModel = new Rating(rating)
+  await ratingModel.save()
+  res.json(ratingModel)
 })
 
 app.listen(port, () => {
