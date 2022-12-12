7b56da5ce5c120198c8654ebb36267dd319e07d8
NeilGGopal <gopaln@purdue.edu>
2022-12-12 01:00:23 -0500

    Added an update button

--- react-app/src/components/Planner.js
+++ react-app/src/components/Planner.js
@@ -120,41 +120,14 @@ export default class Planner extends Component {
           </fieldset>
           <br></br>
           <button class="btn btn-outline-danger" type="submit" onClick={() => (this.setState({ button: 1 }))}>Remove Movie</button>
+          &nbsp; &nbsp;
           <button class="btn btn-outline-danger" type="submit" onClick={() => (this.setState({ button: 2 }))}>Add Movie</button>
+          &nbsp; &nbsp;
+          <button class="btn btn-outline-danger" type="submit" onClick={() => (this.setState({ button: 3 }))}>Update Movie</button>
         </form>
       </div>
     )
   }
 }
-
-/**
-function Planner() {
-  return (
-    <div className="planner">
-        <br></br>
-        <header className="hdr">Planner</header>
-         <br></br>
-
-         &nbsp;
-         &nbsp;
-          <form>
-          <fieldset className="form-group" style={{marginLeft:"500px", marginRight:"500px"}}>
-              <input type="text" className="form-control" placeholder="Enter movie name..." />
-            </fieldset>
-            <br></br>
-            <fieldset className="form-group" style={{marginLeft:"500px", marginRight:"500px"}}>
-              <input type="text" className="form-control" placeholder="Enter movie length (minutes)" />
-            </fieldset>
-            <br></br>
-            <fieldset className="form-group" style={{marginLeft:"500px", marginRight:"500px"}}>
-              <input type="text" className="form-control" placeholder="Enter movie year" />
-            </fieldset>
-            <br></br>
-          </form>
-         <button class="btn btn-outline-danger">Add Movie</button>
-    </div>
-  ); 
-}
-*/
 
 // export default Planner;
